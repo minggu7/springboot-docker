bdd608e4780851dcea18d423cde71121da6d5499
kmg2388 <a01065732388@gmail.com>
2024-09-12 13:59:25 +0900

    feat: project build

---
+++ src/test/java/com/example/ASP/AspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ASP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
